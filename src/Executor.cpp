9ee87f5e729d5ce850c400e2423bfda39e1f8250
Ivan Sukhanov <ivan261104@gmail.com>
2025-05-19 11:19:41 +0100

    fork/wait draft

--- src/Executor.cpp
+++ src/Executor.cpp
@@ -1,9 +1,23 @@
 #include "Executor.hpp"
 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+#include <iostream>
 
 Executor::Executor() = default;
 
-void Executor::execute(Command &cmd) const {
-  // TODO
+void Executor::execute(Command& cmd) const {
+  pid_t pid = fork();
+
+  if (pid == 0) {
+    std::cout << "child" << '\n';
+  } else if (pid > 0) {
+    int* status;
+    waitpid(pid, status, 0);
+    std::cout << "parent, wait status: " << *status;
+  } else {
+    std::cerr << "fork failed";
+  }
 }
