94314b37a995a49989130213c0563a2d15fa87df
Ivan Sukhanov <ivan261104@gmail.com>
2025-05-20 05:07:39 +0300

    parse function was worked on

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -15,16 +16,34 @@ std::vector<std::unique_ptr<Command>> Parser::parse(const std::string &line) {
   auto res = find_first_of(begin(line), end(line), begin(spaceSymbols),
                            end(spaceSymbols));
   int pos = 0;
+  vector<string> pieces;  // consinder using llist
   while (res != end(line)) {
     // cout << "matched: #" << *res << "#\n";
-    int pieceLength = distance(begin(line) + pos, res);
-    string piece = line.substr(pos, pieceLength);
-    pos = distance(begin(line), res) + 1;
+    int resDistance = distance(begin(line), res);
+
+    string piece = line.substr(pos, resDistance - pos);
+    if (!piece.empty()) {
+      pieces.push_back(piece);
+    }
+
+    pos = resDistance + 1;
     res = find_first_of(next(res), end(line), begin(spaceSymbols),
                         end(spaceSymbols));
+
+    // debug
     if (piece.empty()) continue;
-    cout << "pl: " << pieceLength << "\npiece: #" << piece << "#\npos " << pos
-         << "\nres " << res - begin(line) << '\n';
+    cout << "piece: #" << piece << "#\npos " << pos << "\nres "
+         << res - begin(line) << '\n';
   }
+
+  if (string lastPiece = line.substr(pos, line.size() - pos);
+      !lastPiece.empty()) {
+    pieces.push_back(lastPiece);
+  }
+
+  copy(begin(pieces), end(pieces),
+       ostream_iterator<string>{cout, ", "});  // debug
   // TODO aslo add the last piece
+  unique_ptr<Command> parsedCommand = make_unique<Command>(
+      pieces.front(), vector<string>{begin(pieces) + 1, end(pieces)});
 }
