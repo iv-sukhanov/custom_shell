ff8627768a9abd5e5efa933ced5940a4032fc178
Ivan Sukhanov <ivan261104@gmail.com>
2025-05-25 02:49:12 +0300

    Command class was commented

--- include/Command.hpp
+++ include/Command.hpp
@@ -1,17 +1,71 @@
+/**
+ * @file Command.hpp
+ * @brief Contains a Command class
+ * @author Sukhanov Ivan
+ * @date 25/05/2025
+ * @version 1.0
+ */
+
 #pragma once
 
 #include <optional>
 #include <string>
 #include <vector>
 
+/**
+ * @class Command
+ * @brief The class represents a single command.
+ *
+ * The class contains all the information about a single command including name, arguments, file of the
+ * redirected output, and flag denoting if the command was ment to run in backgroung.
+ * All the methods are getters/setters, so its use case is just to passively store the data.
+ *
+ * @author Sukhanov Ivan
+ * @date 25/05/2025
+ */
 class Command {
    public:
+    /**
+     * @brief Constructs a Command with a name and arguments.
+     * @param name The name of the command.
+     * @param args The arguments for the command.
+     */
     Command(const std::string& name, const std::vector<std::string>& args);
+
+    /**
+     * @brief Gets the name of the command.
+     * @return The command name.
+     */
     std::string getName() const;
+
+    /**
+     * @brief Gets the arguments of the command.
+     * @return The command arguments.
+     */
     std::vector<std::string> getArgs() const;
+
+    /**
+     * @brief Sets the file to which the command's output should be redirected.
+     * @param file The output file path.
+     */
     void setOutputRedirect(const std::string& file);
+
+    /**
+     * @brief Gets the output redirection file, if set.
+     * @return The output file path, or std::nullopt if not set.
+     */
     std::optional<std::string> getOutputRedirect() const;
-    void setParallel(bool);
+
+    /**
+     * @brief Sets whether the command should run in parallel.
+     * @param parallel True to run in parallel, false otherwise.
+     */
+    void setParallel(bool parallel);
+
+    /**
+     * @brief Checks if the command is set to run in parallel.
+     * @return True if parallel, false otherwise.
+     */
     bool isParallel() const;
 
    private:
--- src/Command.cpp
+++ src/Command.cpp
@@ -1,34 +1,75 @@
+/**
+ * @file Command.cpp
+ * @brief File implemets Command class methods
+ *
+ * Most of the functions are getters/setters, everything should be clear.
+ *
+ * @author Sukhanov Ivan
+ * @date 25/5/2025
+ * @version 1.0
+ */
+
 #include "Command.hpp"
 
 #include <string>
 #include <utility>
 #include <vector>
 
+/**
+ * @brief Constructs a Command object with the given name and arguments.
+ *
+ * @param name The name of the command.
+ * @param args The arguments for the command.
+ */
 Command::Command(const std::string& name, const std::vector<std::string>& args) {
     this->name = name;
     this->args = args;
 }
 
+/**
+ * @brief Gets the name of the command.
+ * @return The command name as a string.
+ */
 std::string Command::getName() const {
     return this->name;
 }
 
+/**
+ * @brief Gets the arguments for the command.
+ * @return A vector of argument strings.
+ */
 std::vector<std::string> Command::getArgs() const {
     return this->args;
 }
 
+/**
+ * @brief Sets the file to which the command's output should be redirected.
+ * @param file The output file path.
+ */
 void Command::setOutputRedirect(const std::string& file) {
     this->outputFile = file;
 }
 
+/**
+ * @brief Gets the output redirection file, if set.
+ * @return An optional string containing the output file path.
+ */
 std::optional<std::string> Command::getOutputRedirect() const {
     return this->outputFile;
 }
 
+/**
+ * @brief Sets whether the command should run in parallel.
+ * @param inParallel True if the command should run in parallel, false otherwise.
+ */
 void Command::setParallel(bool inParallel) {
     this->inParallel = inParallel;
 }
 
+/**
+ * @brief Checks if the command is set to run in parallel.
+ * @return True if the command runs in parallel, false otherwise.
+ */
 bool Command::isParallel() const {
     return this->inParallel;
 }
