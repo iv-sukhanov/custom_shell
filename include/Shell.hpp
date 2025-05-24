84cca7bc000ddf9e29b7a1a5259e4020f162acb4
Ivan Sukhanov <ivan261104@gmail.com>
2025-05-25 02:58:05 +0300

    Shell class was commented

--- include/Shell.hpp
+++ include/Shell.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file Shell.hpp
+ * @brief Contains a Shell class
+ * @author Sukhanov Ivan
+ * @date 25/05/2025
+ * @version 1.0
+ */
 #pragma once
 
 #include <memory>
@@ -7,20 +14,66 @@
 #include "Executor.hpp"
 #include "Parser.hpp"
 
+/**
+ * @class Shell
+ * @brief The class represents a shell that can execute commands interactively or from a file.
+ *
+ * The Shell class provides functionality to run commands either interactively through user input or by
+ * reading commands from a specified file. It uses a parser to interpret the commands and an executor to run
+ * them. It displays a prompt to the user and handles input lines, processing them into commands that can be
+ * executed.
+ *
+ * @author Sukhanov Ivan
+ * @date 25/05/2025
+ */
 class Shell {
    public:
+    /**
+     * @brief Constructs a new Shell instance.
+     */
     Shell();
+
+    /**
+     * @brief Runs the shell interactively.
+     */
     void run();
+
+    /**
+     * @brief Runs the shell with commands from the specified file.
+     * @param filename The path to the file containing shell commands.
+     */
     void run(const std::string& filename);
 
    private:
+    /**
+     * @brief The prompt title displayed to the user.
+     */
     static const char* PROMPT_TITLE;
 
+    /**
+     * @brief Handles a single line of user input.
+     * @param line The input line to process.
+     */
     void handleInputLine(const std::string& line);
 
+    /**
+     * @brief Reads a line of input from the user.
+     * @return The input line as a string.
+     */
     static std::string readInput();
+
+    /**
+     * @brief Displays the shell prompt to the user.
+     */
     static void displayPrompt();
 
+    /**
+     * @brief Parser for interpreting user input.
+     */
     std::unique_ptr<Parser> parser;
+
+    /**
+     * @brief Executor for running parsed commands.
+     */
     std::unique_ptr<Executor> executor;
 };
--- src/Shell.cpp
+++ src/Shell.cpp
@@ -1,3 +1,18 @@
+/**
+ * @file Shell.cpp
+ * @brief File implemets Shell class
+ *
+ * The Shell class can be run interactively or with commands from a file.
+ * In interactive mode, runs a loop that ends when the user inputs "exit". In batch mode,
+ * reads commands from a file, executes them, and then exits. Once recieved a line (no matter from file of
+ * user), it parses the line into commands and executes them one by one. Parsing and executing are delegated
+ * to Parser and Executor classes respectively.
+ *
+ * @author Sukhanov Ivan
+ * @date 25/5/2025
+ * @version 1.0
+ */
+
 #include "Shell.hpp"
 
 #include <csignal>
@@ -12,13 +27,22 @@
 
 #include "Parser.hpp"
 
+/**
+ * @brief Constructs a new Shell instance, initializing the parser and executor.
+ */
 Shell::Shell() {
     this->parser = std::make_unique<Parser>();
     this->executor = std::make_unique<Executor>();
 };
 
+/**
+ * @brief The prompt title displayed to the user.
+ */
 const char* Shell::PROMPT_TITLE = "ishell";
 
+/**
+ * @brief Runs the shell interactively, reading and executing user input in a loop.
+ */
 void Shell::run() {
     using namespace std;
 
@@ -34,6 +58,10 @@ void Shell::run() {
     }
 }
 
+/**
+ * @brief Runs the shell with commands read from a file.
+ * @param filename The path to the file containing shell commands.
+ */
 void Shell::run(const std::string& filename) {
     using namespace std;
 
@@ -54,6 +82,10 @@ void Shell::run(const std::string& filename) {
     }
 }
 
+/**
+ * @brief Parses and executes a single line of input.
+ * @param line The input line to process.
+ */
 void Shell::handleInputLine(const std::string& line) {
     using namespace std;
 
@@ -81,12 +113,19 @@ void Shell::handleInputLine(const std::string& line) {
     }
 }
 
+/**
+ * @brief Reads a line of input from the user.
+ * @return The input line as a string.
+ */
 std::string Shell::readInput() {
     std::string input;
     std::getline(std::cin, input);
     return input;
 }
 
+/**
+ * @brief Displays the shell prompt to the user.
+ */
 void Shell::displayPrompt() {
     std::cout << Shell::PROMPT_TITLE << "> " << std::flush;
 }
